fc9f197b8dbe1fca4b70ac56915ac95bb64949ef
alkor63 <alkor@medi-spa.ru>
2023-02-23 18:20:42 +0300

    2nd commit

--- src/main/java/pro/sky/budgetapp/controllers/FirstController.java
+++ src/main/java/pro/sky/budgetapp/controllers/FirstController.java
@@ -1,1 +1,25 @@
+package pro.sky.budgetapp.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstController {
+
+    @GetMapping("/")
+    public String helloWorld(){
+        return "Приложение запущено";
+    }
+
+    @GetMapping("/info")
+    public String aboutMe(){
+        return "Алексей Корниенко *** проект BudgetApp *** " +
+                "создан 23.02.2023 *** наше первое вэб-приложение";
+    }
+
+
+//    public String page(@RequestParam String page){
+//        return "input message = " + page;
+//    }
+}
